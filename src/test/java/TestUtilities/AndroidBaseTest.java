89722e0a8114118295affbd6360cc3d343784950
Svale09 <svalinaivan09@gmail.com>
2023-08-29 22:28:47 +0200

    Added iOS and Android apps, implemented the AndroidBaseTest

--- src/test/java/TestUtilities/AndroidBaseTest.java
+++ src/test/java/TestUtilities/AndroidBaseTest.java
@@ -3,8 +3,15 @@ package TestUtilities;
 import Utilities.AppiumUtilities;
 import com.sun.tools.javac.comp.Todo;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import org.testng.annotations.BeforeClass;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
 public class AndroidBaseTest extends AppiumUtilities {
     AppiumDriverLocalService service;
@@ -13,7 +20,24 @@ public class AndroidBaseTest extends AppiumUtilities {
     //TODO add the first visible screen POM as a varaible
 
     @BeforeClass
-    public void ConfigureAppium(){
+    public void ConfigureAppium() throws IOException {
+        Properties properties = new Properties();
+        FileInputStream fileInputStream = new FileInputStream("Resources/AndroidData.properties");
 
+        properties.load(fileInputStream);
+
+        service = startAppiumService(
+                properties.getProperty("ipAddress"),
+                Integer.parseInt(properties.getProperty("port"))
+        );
+
+        UiAutomator2Options options = new UiAutomator2Options();
+        options.setDeviceName(properties.getProperty("deviceName"))
+                .setPlatformVersion(properties.getProperty("platformVersion"))
+                .setAutomationName("UIAutomator2")
+                .setApp("/Users/ivansvalina/IdeaProjects/ZavrsniRad/src/test/java/Resources/Phoenix_Android.apk");
+
+        AndroidDriver driver = new AndroidDriver(options);
+        //TODO add the first visible screen POM for the driver to open it
     }
 }
